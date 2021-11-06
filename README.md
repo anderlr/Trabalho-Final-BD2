05a314e732ea09389589fc74fc2719747dd12a6c
anderlr13 <anderlr13@gmail.com>
2021-11-06 14:16:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Trabalho Final de Banco de Dados 2
+O projeto consiste em uma API que busca os dados da SpaceX-API e
+trata os mesmo pra depois inserir em um banco de dados PostgreSQL
+
+## Equipe
+- Anderson Leandro - 2018019033
+- Ikaro Siqueira - 2019000054
+- Vinicius Rezende - 2019003940
+- Lucas da Silva Marcelo - 2019004868
+- Luiz Eduardo - 2019003136
+
+## Como rodar o projeto
+1. Clone o projeto
+2. Rode o comando `npm install` na pasta do projeto
+3. Rode o comando `npm start`
+
+
+## Api utilizada
+https://github.com/r-spacex/SpaceX-API/tree/master/docs#rspacex-api-docs
+
+
+## Rotas Implementadas para esse projeto
+urlBase = http://localhost:5000
+
+- {urlBase}/rockets 
+    - Retorna um dos foguetes encontrados na API
+
+## Collection de exemplos para o projeto
+https://www.getpostman.com/collections/c323461e1ee5646cc6f3
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+var rockets = require('./rockets');
+app.listen(5000, () => {
+    console.log("Inicializado");
+    console.log(`Servidor rodando localmente em: http://localhost:5000`);
+});
+
+app.use('/rockets', rockets); //rotas para obter foguete
+
+
+
+
+
+//==============================Exemplo de conexão com postgresql==============================
+//IMPORTANTE: Lembrar de inslatar o pacote pg do npm
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     connectionString: `postgres://postgres:root@localhost:5432/injection`,
+// });
+
+// pool.on("connect", () => {
+//     console.log("Online");
+// });
+
+//==============================Exemplos de rotas de requisição com Express==============================
+// app.get("/usuarios", async (req, res) => {
+//     // console.log(req.body.query);
+//     const algo = await pool.query(
+//         `select * from usuarios where username = ${req.body.query}`
+//     );
+//     res.status(200).json(algo.rows);
+// });
+
+// app.post("/usuarios", async (req, res) => {
+//     const { rows: user } = await pool.query(
+//         "INSERT INTO usuarios (username, password) VALUES ($1, $2)",
+//         [req.body.user, req.body.password]
+//     );
+//     res.status(200).send("success");
+// });
