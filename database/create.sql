e4e70bfcc1c995620ec2c3d691b28ced410dcf62
Luiz Martins <luizmartins.prog@gmail.com>
2021-11-09 10:25:08 -0300

    add script to create tables

---
+++ database/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE rockets (
+    id serial PRIMARY KEY,
+    name varchar(100),
+    height real,
+    mass real,
+    stages integer,
+    first_flight date
+);
+
+CREATE TABLE launchpads (
+    id serial PRIMARY KEY,
+    name varchar(100),
+    locality varchar(100),
+    region varchar(100),
+    latitude real NOT NULL,
+    longitude real NOT NULL
+);
+
+CREATE TABLE launches(
+    id serial PRIMARY KEY,
+    flight_number integer NOT NULL,
+    name varchar(100) UNIQUE NOT NULL,
+    date date NOT NULL,
+    success BOOLEAN NOT NULL,
+    id_rocket integer REFERENCES rockets(id) NOT NULL,
+    id_launchpad integer REFERENCES launchpads(id) NOT NULL
+);
+
+CREATE TABLE images (
+    id serial PRIMARY KEY,
+    link text UNIQUE NOT NULL,
+    id_rocket integer REFERENCES rockets(id),
+    id_launchpad integer REFERENCES launchpads(id)
+);
